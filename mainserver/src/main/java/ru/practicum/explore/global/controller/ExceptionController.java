fcbc9b33e5c5ff9a27fe5e500cd0704ef1ef188e
dt68 <destruct.you.down@gmail.com>
2025-06-04 01:29:36 +0300

    commit №1 серверы запущены, проверяем, не было в основном Dockerfile открытия двух доп.портов

--- mainserver/src/main/java/ru/practicum/explore/global/controller/ExceptionController.java
+++ mainserver/src/main/java/ru/practicum/explore/global/controller/ExceptionController.java
@@ -31,8 +31,8 @@ public class ExceptionController {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).contentType(MediaType.APPLICATION_JSON).body(errorMessage);
     }
 
-    @ExceptionHandler(HttpClientErrorException.class)
-    public ResponseEntity<ErrorMessage> handleConflict(final HttpClientErrorException e) {
+    @ExceptionHandler(HttpClientErrorException.Conflict.class)
+    public ResponseEntity<ErrorMessage> handleConflict(final HttpClientErrorException.Conflict e) {
         log.warn("Encountered {}: returning 409 Error. Message: {}", e.getClass().getSimpleName(), e.getMessage());
         ErrorMessage errorMessage = new ErrorMessage(List.of(e.getClass().getSimpleName(), "Data integrity violation"), e.getLocalizedMessage(), "Data integrity violation");
         return ResponseEntity.status(HttpStatus.CONFLICT).contentType(MediaType.APPLICATION_JSON).body(errorMessage);

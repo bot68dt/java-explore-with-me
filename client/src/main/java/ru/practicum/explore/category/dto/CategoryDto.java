dee92d871a0d99463f493cfd816795d763b155ed
dt68 <destruct.you.down@gmail.com>
2025-06-03 12:32:16 +0300

    commit test

--- client/src/main/java/ru/practicum/explore/category/dto/CategoryDto.java
+++ client/src/main/java/ru/practicum/explore/category/dto/CategoryDto.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.UniqueElements;
 
 @Data
 @AllArgsConstructor

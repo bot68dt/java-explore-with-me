e8a2978b57f7003b2d6490d187519419255e0c06
dt68 <destruct.you.down@gmail.com>
2025-06-04 19:36:01 +0300

    commit №1 серверы запущены, проверяем, не было в основном Dockerfile открытия двух доп.портов

--- client/src/main/java/ru/practicum/explore/user/controller/UserController.java
+++ client/src/main/java/ru/practicum/explore/user/controller/UserController.java
@@ -4,7 +4,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.explore.user.client.AdminUserClient;
@@ -12,7 +11,7 @@ import ru.practicum.explore.user.client.UserClient;
 import ru.practicum.explore.user.dto.RequestsDto;
 import ru.practicum.explore.user.dto.UserDto;
 
-@Controller
+@RestController
 @RequestMapping
 @RequiredArgsConstructor
 @Slf4j

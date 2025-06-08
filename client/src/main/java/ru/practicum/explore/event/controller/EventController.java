0c841fe083c9a17ad53392c9cd38477cc4025c99
dt68 <destruct.you.down@gmail.com>
2025-06-08 19:20:45 +0300

    commit №4 правки согласно ревью

--- client/src/main/java/ru/practicum/explore/event/controller/EventController.java
+++ client/src/main/java/ru/practicum/explore/event/controller/EventController.java
@@ -14,7 +14,6 @@ import ru.practicum.explore.event.dto.PatchEventDto;
 import ru.practicum.explore.statistics.client.StatisticsClient;
 import ru.practicum.explore.statistics.dto.StatisticsDto;
 
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.LinkedHashMap;

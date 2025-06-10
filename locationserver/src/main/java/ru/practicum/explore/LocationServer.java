0a252bced1ae517192f608782113c618133c28b6
dt68 <destruct.you.down@gmail.com>
2025-06-10 18:29:54 +0300

    test commit

---
+++ locationserver/src/main/java/ru/practicum/explore/LocationServer.java
@@ -0,0 +1,11 @@
+package ru.practicum.explore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationServer {
+    public static void main(String[] args) {
+        SpringApplication.run(LocationServer.class, args);
+    }
+}
